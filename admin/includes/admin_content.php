e786fb536f81495bd56ad13d257aefc5ebc4905f
Bartek <bartlomiejkuzniar@gmail.com>
2020-06-02 04:08:00 +0200

    Section 5: The User Class

---
+++ admin/includes/admin_content.php
@@ -0,0 +1,39 @@
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+                ADMIN
+                <small>Subheadinggs</small>
+            </h1>
+            <?php
+//                $result_set = User::find_all_users();
+//                while ($row = mysqli_fetch_array($result_set)) {
+//                    echo $row['username'] . "<br>";
+//                }
+//                $found_user = User::find_user_by_id(2);
+//                $user = User::instantiation($found_user);
+//
+//                echo $user->id;
+//                $users = User::find_all_users();
+//                foreach($users as $user){
+//                    echo $user->id . "<br>";
+//                }
+            $found_user = User::find_user_by_id(2);
+            echo $found_user->username;
+            $picture = new Picture();
+            ?>
+            <ol class="breadcrumb">
+                <li>
+                    <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
+                </li>
+                <li class="active">
+                    <i class="fa fa-file"></i> Blank Page
+                </li>
+            </ol>
+        </div>
+    </div>
+    <!-- /.row -->
+
+</div>
---
+++ admin/includes/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Database Connection Constans
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'gallery_db');
+
